84c4a64fd54da3ec5b7179988983cf4da3f3f818
mensur056 <mansur.sarxanov@gmail.com>
2021-11-29 21:35:15 +0400

    test

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 
 class ChatScreen extends StatefulWidget {
+  static String id='chat_screen';
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
