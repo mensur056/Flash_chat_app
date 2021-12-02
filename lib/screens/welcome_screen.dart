0dd108c357e4d6415b706f8dd07c34c7de66e044
mensur056 <mansur.sarxanov@gmail.com>
2021-12-02 19:34:47 +0400

    test

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flashchat/components/Rounded_button.dart';
 import 'package:flashchat/screens/login_screen.dart';
 import 'package:flashchat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
@@ -88,31 +89,4 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   }
 }
 
-class RoundedButton extends StatelessWidget {
-  final Color colour;
-  final String title;
-  final VoidCallback onPressed;
 
-  const RoundedButton(
-      {required this.colour, required this.title, required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: Material(
-        elevation: 5.0,
-        color: colour,
-        borderRadius: BorderRadius.circular(30.0),
-        child: MaterialButton(
-          onPressed: onPressed,
-          minWidth: 200.0,
-          height: 42.0,
-          child: Text(
-            title,
-          ),
-        ),
-      ),
-    );
-  }
-}
