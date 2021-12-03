c9642e1361511428424a730d9f39cf949e307f96
mensur056 <mansur.sarxanov@gmail.com>
2021-12-03 18:49:27 +0400

    test

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,7 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
-        ),
-      ),
+
      initialRoute: WelcomeScreen.id,
       routes: {
         WelcomeScreen.id:(context)=>WelcomeScreen(),
